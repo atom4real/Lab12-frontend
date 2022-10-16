ed1038b480a5b6a736b0f169a4cb756aa24a9982
atomfaster@gmail.com <atomfaster@gmail.com>
2022-10-16 22:47:53 +0700

    2.10

--- src/views/event/EventDetailView.vue
+++ src/views/event/EventDetailView.vue
@@ -1,6 +1,7 @@
 <template>
   <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
   <p>{{ event.description }}</p>
+  <img v-for="url in event.imageUrls" :key="url" :src="url" alt="img" />
 </template>
 
 <script>
